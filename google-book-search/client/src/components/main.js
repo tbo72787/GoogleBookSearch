1b6d8fe4c27895c7c1fda5c0913214cc3433b00d
Taylor Zettel <tzettel@gmail.com>
2020-03-07 12:04:29 -0500

    fixed icon sizes in manifest.json to fix error

--- google-book-search/client/src/components/main.js
+++ google-book-search/client/src/components/main.js
@@ -1,1 +1,32 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import search from "./search";
+import saved from "./saved";
+import nav from "./nav/nav";
+import "../App.css"
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav/>
+          <TitleCard/>
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
--- google-book-search/server.js
+++ google-book-search/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+var mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +11,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/google-book-search", { useNewUrlParser: true });
+
 
 // Define API routes here
 
